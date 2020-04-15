045ac9cc81b0299235925200d823bdc3e8bfa863
znax12 <58237276+znax12@users.noreply.github.com>
2020-04-15 18:10:01 +0200

    Add files via upload

---
+++ WeaponDamage/client.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+    while true do
+    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_PISTOL"), 0.07) 
+    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_UNARMED"), 0.2)   
+	Wait(0)
+    end
+end)
